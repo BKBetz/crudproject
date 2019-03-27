3b0891f8d6bf7072224c96d33124a5dea73909aa
Brandon Betz <Brandonbetz@live.nl>
2019-03-27 16:39:11 +0100

    added new users app and model

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    birth_date = models.DateField
+    user_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user_name
+
+    class Meta:
+        verbose_name_plural = 'Users'
